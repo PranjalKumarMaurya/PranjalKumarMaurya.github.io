6a14c489613db3b6994351edf6f6985b6570c7d6
Pranjal Kumar Maurya <workwithpranjal@gmail.com>
2023-04-30 06:09:59 +0530

    home page done

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation container'),
+    path: 'https://assets1.lottiefiles.com/packages/lf20_mK7qUzkwYA.json',
+    render: 'svg',
+    loop: true,
+    autoplay: true
+})
